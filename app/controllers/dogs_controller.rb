4b14cc137102f7d7ecc624c784c743d52f164051
Alex Williams <alex_williams@protonmail.com>
2019-12-19 14:53:52 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,36 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+    def show
+        @dog = Dog.find(params['id'])
+        @owners = Employee.select{|m| m.dog == @dog.name}
+    end
+    def new
+        @dog = Dog.new
+    end
+    def create
+        @dog = Dog.new(dog_params)
+        if @dog.valid?
+            redirect_to dog_path(@dog)
+        else
+            render :new
+        end
+    end
+    def edit
+        @dog = Dog.find(params['id'])
+    end
+    def update
+        @dog = Dog.update(dog_params['id'], dog_params)
+        if @dog.valid?
+            redirect_to dog_path(@dog)
+        else
+            render :edit
+        end
+    end
+    def delete
+        Dog.find(params['id']).destroy
+        redirect_to dog_path()
+    end
+    private
 end
