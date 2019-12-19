5e1c74fccab5b7d27b6a3f8cd514e6b6e6c118d0
MACorman <madelinecorman@gmail.com>
2019-12-19 12:02:09 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+    validates :first_name, :last_name, :title, :office, presence: true
 end
